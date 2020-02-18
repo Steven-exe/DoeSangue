cea5a155bf75e8f240bd0701d4b1581d312874c5
Steven-exe <estevansantosaquino@hotmail.com>
2020-02-18 14:09:53 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+// logica de fazer café
+
+const cor= "branco";
+const tamanho = 2.5;
+
+function VerCopoSujo() {
+    return "Deu certo";
+    //logica para verificar se o copo esta sujo
+}
+
+
+
+const copo = {
+    
+    cor,
+    tamanho,
+    VerCopoSujo
+
+}
+
+console.log(copo.VerCopoSujo());
